0bf71a2a3bfeb83359802b42670b4bdbc50d49f6
tracer-nju <tracer@nju.edu.cn>
2024-07-18 17:18:34 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:18:34 up 16:30,  0 users,  load average: 0.16, 0.30, 0.37

---
+++ .shadow/pstree.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <ctype.h>
+
+#define PROC_DIR "/proc"
+
+typedef struct Process {
+  int pid;
+  struct Process *child;
+} Process;
+
+int is_int(char *str) {
+  while (*str) {
+    if (!isdigit(*str)) {
+      return 1;
+      str++;
+    }
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    assert(argv[i]);
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  assert(!argv[argc]);
+  printf("%s\n", PROC_DIR);
+
+  DIR *proc = opendir(PROC_DIR);
+  if (!proc) {
+    perror("opendir");
+    return EXIT_FAILURE;
+  }
+
+  struct dirent *entry;
+  while ((entry = readdir(proc)) != NULL) {
+    if (entry->d_type == DT_DIR) {
+      printf("%s - %d\n - %d", entry->d_name, entry->d_type, is_int(entry->d_name));
+      // int pid = atoi(entry->d_name);
+      // int ppid;
+      // char name[256];
+      // get_process_info(pid, &ppid, name, sizeof(name));
+      // Process *proc = create_process(pid, ppid, name);
+      // processes[pid] = proc;
+      // if (ppid >= 0 && processes[ppid]) {
+      //   add_child_process(processes[ppid], proc);
+      // } else {
+      //   add_child_process(root, proc);
+      // }
+    }
+  }
+
+  return 0;
+}
