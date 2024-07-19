9e2819afac79c62b98fa4e10c359e00bca37c2c4
tracer-nju <tracer@nju.edu.cn>
2024-07-19 17:49:22 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:49:22 up 12:40,  0 users,  load average: 1.24, 0.74, 0.68

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -27,25 +27,46 @@ Process *new_process(int pid) {
   Process *proc = malloc(sizeof(Process));
   proc->pid = pid;
   int cap = 8;
-  proc->child_arr = malloc(sizeof(Process*)*cap);
+  proc->child_arr = malloc(sizeof(Process *) * cap);
   proc->child_arr_cap = 8;
   proc->child_arr_len = 0;
 
   return proc;
 }
 
-void process_printf(Process* proc) {
+void process_printf(Process *proc) {
   printf("pid: %d\n", proc->pid);
+  for (int i = 0; i < proc->child_arr_len; i++) {
+    process_printf(proc->child_arr[i]);
+  }
 }
 
 void add_child_proc(Process *proc, Process *child) {
   if (proc->child_arr_len == proc->child_arr_cap) {
     proc->child_arr_cap *= 2;
-    proc->child_arr = (Process**)realloc(proc->child_arr, sizeof(Process*) * proc->child_arr_cap);
+    proc->child_arr = (Process **)realloc(
+        proc->child_arr, sizeof(Process *) * proc->child_arr_cap);
   }
 
   proc->child_arr_len++;
   proc->child_arr[proc->child_arr_len] = child;
+}
+
+size_t parse_ppid(int pid) {
+  char path[256];
+  snprintf(path, sizeof(path), "/proc/%d/status", pid);
+  FILE *file = fopen(path, "r");
+  if (!file) {
+    perror("fopen error");
+    return -1;
+  }
+
+  int ppid;
+  fscanf(file, "%*d %*s %*c %d", &ppid); // 读取并跳过不需要的字段
+  fclose(file);
+
+  printf("%d", ppid)
+  return ppid;
 }
 
 int main(int argc, char *argv[]) {
@@ -67,7 +88,9 @@ int main(int argc, char *argv[]) {
   while ((entry = readdir(proc_dir)) != NULL) {
     if (entry->d_type == DT_DIR && is_int(entry->d_name)) {
       int pid = atoi(entry->d_name);
+      size_t ppid = parse_ppid(pid);
       Process *proc = new_process(pid);
+      add_child_proc(proc_arr[ppid], proc);
       proc_arr[pid] = proc;
     }
   }
