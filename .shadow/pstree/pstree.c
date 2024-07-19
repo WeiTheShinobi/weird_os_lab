c17a5b38f01ec1916680a669baf8ff8ebf660d8a
tracer-nju <tracer@nju.edu.cn>
2024-07-19 18:00:09 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:00:09 up 12:50,  0 users,  load average: 1.19, 0.96, 0.76

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -91,9 +91,9 @@ int main(int argc, char *argv[]) {
   while ((entry = readdir(proc_dir)) != NULL) {
     if (entry->d_type == DT_DIR && is_int(entry->d_name)) {
       int pid = atoi(entry->d_name);
-      size_t ppid = parse_ppid(pid);
+      // size_t ppid = parse_ppid(pid);
       Process *proc = new_process(pid);
-      add_child_proc(proc_arr[ppid], proc);
+      // add_child_proc(proc_arr[ppid], proc);
       proc_arr[pid] = proc;
     }
   }
