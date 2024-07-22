735cb915dcfa00c0d9feca984465ca429594d8e3
tracer-nju <tracer@nju.edu.cn>
2024-07-22 18:01:48 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:48 up  8:07,  0 users,  load average: 0.76, 0.60, 0.55

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -54,9 +54,9 @@ void splash() {
 
 // Operating system is a C program!
 int main(const char *args) {
-  puts("mainargs = \"");
   ioe_init();
 
+  puts("mainargs = \"");
   puts(args);  // make run mainargs=xxx
   puts("\"\n");
 
