5508bf1fa79917684fdb27498f8222de2e5367fe
tracer-nju <tracer@nju.edu.cn>
2024-07-22 18:42:49 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:42:48 up  8:48,  0 users,  load average: 0.61, 0.85, 0.81

--- .shadow/abstract-machine/klib/src/stdlib.c
+++ .shadow/abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,6 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -8,25 +8,39 @@ static unsigned long int next = 1;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+void srand(unsigned int seed) { next = seed; }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+int abs(int x) { return (x < 0 ? -x : x); }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
+  while (*nptr == ' ') {
+    nptr++;
+  }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
+}
+
+void itoa(int n, void* ss) {
+  char *s = ss;
+  int i, j, sign;
+  if ((sign = n) < 0) // 記錄符號
+    n = -n;           // 使n成為正數
+  i = 0;
+  do {
+    s[i++] = n % 10 +'0'; // 取下一個數字
+  } while ((n /= 10) > 0); // 刪除該數字
+  if (sign < 0)
+    s[i++] ='-';
+  s[i] ='\0';
+  for (j = i; j >= 0; j--) // 生成的數字是逆序的，所以要逆序輸出
+    printf("%c", s[j]);
 }
 
 void *malloc(size_t size) {
@@ -39,7 +53,6 @@ void *malloc(size_t size) {
   return NULL;
 }
 
-void free(void *ptr) {
-}
+void free(void *ptr) {}
 
 #endif
