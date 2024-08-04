0df1bf9aed203e76af86053d0054a531c79528f5
tracer-nju <tracer@nju.edu.cn>
2024-08-04 15:22:39 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:22:39 up 13:22,  0 users,  load average: 1.05, 0.73, 0.62

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -6,32 +6,32 @@
 
 #
 typedef struct context {
-  uint64_t rax;
-  uint64_t rbx;
-  uint64_t rcx;
-  uint64_t rdx;
-  uint64_t rsi;
-  uint64_t rdi;
-  uint64_t rbp;
-  uint64_t rsp;
-  uint64_t r8;
-  uint64_t r9;
-  uint64_t r10;
-  uint64_t r11;
-  uint64_t r12;
-  uint64_t r13;
-  uint64_t r14;
-  uint64_t r15;
-  uint64_t rip;
-  uint64_t eflags;
-  uint64_t cs;
-  uint64_t ss;
-  uint64_t ds;
-  uint64_t es;
-  uint64_t fs;
-  uint64_t gs;
-  uint64_t fs_base;
-  uint64_t gs_base;
+  size_t rax;
+  size_t rbx;
+  size_t rcx;
+  size_t rdx;
+  size_t rsi;
+  size_t rdi;
+  size_t rbp;
+  size_t rsp;
+  size_t r8;
+  size_t r9;
+  size_t r10;
+  size_t r11;
+  size_t r12;
+  size_t r13;
+  size_t r14;
+  size_t r15;
+  size_t rip;
+  size_t eflags;
+  size_t cs;
+  size_t ss;
+  size_t ds;
+  size_t es;
+  size_t fs;
+  size_t gs;
+  size_t fs_base;
+  size_t gs_base;
 } context;
 
 context* new_context() {
@@ -109,32 +109,32 @@ char *context_to_string(context *cx) {
   assert(buffer != NULL);
 
   snprintf(buffer, buffer_size,
-           "rax: 0x%016lu\n"
-           "rbx: 0x%016lu\n"
-           "rcx: 0x%016lu\n"
-           "rdx: 0x%016lu\n"
-           "rsi: 0x%016lu\n"
-           "rdi: 0x%016lu\n"
-           "rbp: 0x%016lu\n"
-           "rsp: 0x%016lu\n"
-           "r8:  0x%016lu\n"
-           "r9:  0x%016lu\n"
-           "r10: 0x%016lu\n"
-           "r11: 0x%016lu\n"
-           "r12: 0x%016lu\n"
-           "r13: 0x%016lu\n"
-           "r14: 0x%016lu\n"
-           "r15: 0x%016lu\n"
-           "rip: 0x%016lu\n"
-           "eflags: 0x%016lu\n"
-           "cs: 0x%016lu\n"
-           "ss: 0x%016lu\n"
-           "ds: 0x%016lu\n"
-           "es: 0x%016lu\n"
-           "fs: 0x%016lu\n"
-           "gs: 0x%016lu\n"
-           "fs_base: 0x%016lu\n"
-           "gs_base: 0x%016lu\n",
+           "rax: 0x%016zu\n"
+           "rbx: 0x%016zu\n"
+           "rcx: 0x%016zu\n"
+           "rdx: 0x%016zu\n"
+           "rsi: 0x%016zu\n"
+           "rdi: 0x%016zu\n"
+           "rbp: 0x%016zu\n"
+           "rsp: 0x%016zu\n"
+           "r8:  0x%016zu\n"
+           "r9:  0x%016zu\n"
+           "r10: 0x%016zu\n"
+           "r11: 0x%016zu\n"
+           "r12: 0x%016zu\n"
+           "r13: 0x%016zu\n"
+           "r14: 0x%016zu\n"
+           "r15: 0x%016zu\n"
+           "rip: 0x%016zu\n"
+           "eflags: 0x%016zu\n"
+           "cs: 0x%016zu\n"
+           "ss: 0x%016zu\n"
+           "ds: 0x%016zu\n"
+           "es: 0x%016zu\n"
+           "fs: 0x%016zu\n"
+           "gs: 0x%016zu\n"
+           "fs_base: 0x%016zu\n"
+           "gs_base: 0x%016zu\n",
            cx->rax, cx->rbx, cx->rcx, cx->rdx, cx->rsi, cx->rdi, cx->rbp,
            cx->rsp, cx->r8, cx->r9, cx->r10, cx->r11, cx->r12, cx->r13, cx->r14,
            cx->r15, cx->rip, cx->eflags, cx->cs, cx->ss, cx->ds, cx->es, cx->fs,
