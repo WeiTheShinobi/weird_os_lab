117873c7847d41fa29b62e10b9978b179597cdb0
tracer-nju <tracer@nju.edu.cn>
2024-08-04 15:40:15 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:40:15 up 13:39,  0 users,  load average: 0.78, 1.01, 0.86

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -87,7 +87,8 @@ void context_save(context *cx) {
                : "=m"(cx->rax), "=m"(cx->rbx), "=m"(cx->rcx), "=m"(cx->rdx),
                  "=m"(cx->rsi), "=m"(cx->rdi), "=m"(cx->rbp), "=m"(cx->rsp),
                  "=m"(cx->r8), "=m"(cx->r9), "=m"(cx->r10), "=m"(cx->r11),
-                 "=m"(cx->r12), "=m"(cx->r13), "=m"(cx->r14), "=m"(cx->r15));
+                 "=m"(cx->r12), "=m"(cx->r13), "=m"(cx->r14), "=m"(cx->r15),
+                 "=a"(cx->rip));
 }
 
 char *context_to_string(context *cx) {
