af2cff5f1e1654f6b29613877f61f26fa4e05ab6
tracer-nju <tracer@nju.edu.cn>
2024-08-06 17:05:45 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:05:45 up  1:46,  0 users,  load average: 0.48, 0.74, 0.80

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -1,40 +1,57 @@
 #include "co.h"
 #include "assert.h"
+#include <setjmp.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ucontext.h>
-#include <setjmp.h>
 
-typedef struct {
-  ucontext_t context;
-} context;
+typedef struct CONODE {
+	struct co *coroutine;
 
-context *new_context() {
-  context *ctx = malloc(sizeof(context));
-  if (!ctx) {
-    perror("malloc");
-    exit(EXIT_FAILURE);
-  }
-  return ctx;
+	struct CONODE *fd, *bk;
+} CoNode;
+
+
+static CoNode *co_node = NULL;
+static CoNode *current = NULL;
+
+static void co_node_insert(struct co *coroutine) {
+	CoNode *victim = (CoNode*)malloc(sizeof(CoNode));
+	assert(victim);
+
+
+	victim->coroutine = coroutine;
+	if(co_node == NULL) {
+		victim->fd = victim->bk = victim;
+		co_node = victim;
+	}else {
+		victim->fd = co_node->fd;
+		victim->bk = co_node;
+		victim->fd->bk = victim->bk->fd = victim;
+	}
 }
 
-void save_context(context *ctx) {
-  if (getcontext(&ctx->context) == -1) {
-    perror("getcontext");
-    exit(EXIT_FAILURE);
-  }
+static CoNode *co_node_remove() {
+	CoNode *victim = NULL;
+
+	if(co_node == NULL) { return NULL; }
+	else if(co_node->bk == co_node) {
+		victim = co_node;
+		co_node = NULL;
+	}else {
+		victim = co_node;
+
+		co_node = co_node->bk;
+		co_node->fd = victim->fd;
+		co_node->fd->bk = co_node;
+	}
+
+	return victim;
 }
 
-void restore_context(context *ctx) {
-  if (setcontext(&ctx->context) == -1) {
-    perror("setcontext");
-    exit(EXIT_FAILURE);
-  }
-}
-
-#define STACK_SIZE 100
+#define STACK_SIZE 1024 * 12
 
 enum co_status {
   CO_NEW = 1, // 新创建，还未执行过
@@ -44,25 +61,44 @@ enum co_status {
 };
 
 struct co {
-  char *name;
+  const char *name;
   void (*func)(void *); // co_start 指定的入口地址和参数
   void *arg;
 
   enum co_status status;     // 协程的状态
   struct co *waiter;         // 是否有其他协程在等待当前协程
-  struct context *context;   // 寄存器现场
+  jmp_buf *context;          // 寄存器现场
   uint8_t stack[STACK_SIZE]; // 协程的堆栈
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   struct co *new_co = malloc(sizeof(struct co));
-  assert(new_co);
-    new_co->name = strdup(name);
-    new_co->func = func;
-    new_co->arg = arg;
-  return NULL;
+  assert(new_co != NULL);
+
+  new_co->name = name;
+  new_co->func = func;
+  new_co->arg = arg;
+  new_co->status = CO_NEW;
+  new_co->waiter = NULL;
+
+  co_node_insert(new_co);
+
+  return new_co;
 }
 
 void co_wait(struct co *co) {}
 
 void co_yield () {}
+
+static inline void stack_switch_call(void *sp, void *entry, void *arg) {
+  asm volatile("movq %%rcx, 0(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+               :
+               : "b"((uintptr_t)sp - 16), "d"((uintptr_t)entry),
+                 "a"((uintptr_t)arg));
+}
+/*
+ * 从调用的指定函数返回，并恢复相关的寄存器。此时协程执行结束，以后再也不会执行该协程的上下文。这里需要注意的是，其和上面并不是对称的，因为调用协程给了新创建的选中协程的堆栈，则选中协程以后就在自己的堆栈上执行，永远不会返回到调用协程的堆栈。
+ */
+static inline void restore_return() {
+  asm volatile("movq 0(%%rsp), %%rcx" : :);
+}
