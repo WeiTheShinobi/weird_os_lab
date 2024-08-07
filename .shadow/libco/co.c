80e2021ec4542e99c38efb842e4d668f9a16ad76
tracer-nju <tracer@nju.edu.cn>
2024-08-07 14:14:57 +0000

    Linux 4a2d57991b38 5.15.49-linuxkit #1 SMP PREEMPT Tue Sep 13 07:51:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:14:57 up 13:56,  0 users,  load average: 1.06, 0.76, 0.53

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -7,48 +7,64 @@
 #include <string.h>
 #include <ucontext.h>
 
-typedef struct CONODE {
-	struct co *coroutine;
+static __attribute__((constructor)) void co_constructor(void) {
+  current = co_start("main", NULL, NULL);
+  current->status = CO_RUNNING;
+}
 
-	struct CONODE *fd, *bk;
+static __attribute__((destructor)) void co_destructor(void) {
+  if (co_node == NULL) {
+    return;
+  }
+
+  while (co_node) {
+    current = co_node->coroutine;
+    free(current);
+    free(co_node_remove());
+  }
+}
+
+typedef struct CONODE {
+  struct co *coroutine;
+
+  struct CONODE *fd, *bk;
 } CoNode;
 
-
 static CoNode *co_node = NULL;
-static CoNode *current = NULL;
+struct co *current;
 
 static void co_node_insert(struct co *coroutine) {
-	CoNode *victim = (CoNode*)malloc(sizeof(CoNode));
-	assert(victim);
+  CoNode *victim = (CoNode *)malloc(sizeof(CoNode));
+  assert(victim);
 
-
-	victim->coroutine = coroutine;
-	if(co_node == NULL) {
-		victim->fd = victim->bk = victim;
-		co_node = victim;
-	}else {
-		victim->fd = co_node->fd;
-		victim->bk = co_node;
-		victim->fd->bk = victim->bk->fd = victim;
-	}
+  victim->coroutine = coroutine;
+  if (co_node == NULL) {
+    victim->fd = victim->bk = victim;
+    co_node = victim;
+  } else {
+    victim->fd = co_node->fd;
+    victim->bk = co_node;
+    victim->fd->bk = victim->bk->fd = victim;
+  }
 }
 
 static CoNode *co_node_remove() {
-	CoNode *victim = NULL;
+  CoNode *victim = NULL;
 
-	if(co_node == NULL) { return NULL; }
-	else if(co_node->bk == co_node) {
-		victim = co_node;
-		co_node = NULL;
-	}else {
-		victim = co_node;
+  if (co_node == NULL) {
+    return NULL;
+  } else if (co_node->bk == co_node) {
+    victim = co_node;
+    co_node = NULL;
+  } else {
+    victim = co_node;
 
-		co_node = co_node->bk;
-		co_node->fd = victim->fd;
-		co_node->fd->bk = co_node;
-	}
+    co_node = co_node->bk;
+    co_node->fd = victim->fd;
+    co_node->fd->bk = co_node;
+  }
 
-	return victim;
+  return victim;
 }
 
 #define STACK_SIZE 1024 * 12
@@ -67,7 +83,7 @@ struct co {
 
   enum co_status status;     // 协程的状态
   struct co *waiter;         // 是否有其他协程在等待当前协程
-  jmp_buf *context;          // 寄存器现场
+  jmp_buf context;           // 寄存器现场
   uint8_t stack[STACK_SIZE]; // 协程的堆栈
 };
 
@@ -87,10 +103,32 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 }
 
 void co_wait(struct co *co) {
+  if (co->status != CO_DEAD) {
+    current->status = CO_WAITING;
+    co->waiter = current;
+    co_yield ();
+  }
+
+  free(co);
   free(co_node_remove());
 }
 
-void co_yield () {}
+#define __LONG_JUMP_STATUS (1)
+void co_yield () {
+  int status = setjmp(current->context);
+  if (status == 0) {
+    co_node = co_node->bk;
+    while (!((current = co_node->coroutine)->status == CO_NEW ||
+           current->status == CO_RUNNING)) {
+      co_node = co_node->bk;
+    }
+    assert(current);
+
+    if (current->status == CO_RUNNING) 
+
+  } else {
+  }
+}
 
 static inline void stack_switch_call(void *sp, void *entry, void *arg) {
   asm volatile("movq %%rcx, 0(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
